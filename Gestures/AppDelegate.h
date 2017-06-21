063af0a1da0ec39d6c43cb9656c0bbedfcee081a
usernamenameuser <writeme@email.ua>
2017-06-21 16:05:14 +0300

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by user on 21.06.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by user on 21.06.17.
+//  Copyright © 2017 iPodium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
